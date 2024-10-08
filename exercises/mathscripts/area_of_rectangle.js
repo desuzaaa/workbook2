c9f4c7b0bd1ad2b0159ae8c1586a1414b97e35e0
Kameron de Suza <z6nekam@gmail.com>
2024-10-08 15:56:43 -0400

    did more exercises

---
+++ exercises/mathscripts/area_of_rectangle.js
@@ -0,0 +1,5 @@
+var length = 32;
+
+var width = 30; 
+
+console.log("The area of the rectangle is " + (length * width));
---
+++ exercises/mathscripts/tip_amount.js
@@ -0,0 +1,6 @@
+var tip = 50;
+
+var total = 2100;
+
+var message = "The tip on the $" + total  + " food bill is $" +  tip
+console.log(message);
