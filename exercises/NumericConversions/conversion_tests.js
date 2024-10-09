36e61673fe79c10885757f5e55187afb09c1a49b
Kameron de Suza <z6nekam@gmail.com>
2024-10-09 13:41:04 -0400

    finished conversion tests

--- exercises/NumericConversions/conversion_tests.js
+++ exercises/NumericConversions/conversion_tests.js
@@ -1,5 +1,5 @@
-var a = parseInt("101.1");
-var b = parseInt("55");
-var c = parseInt("402 Stevens");
-var d = parseInt ("Number 5"); 
+var a = +"101.1";
+var b = +"55";
+var c = +"402 Stevens";
+var d = +"Number 5"; 
 console.log(a,b,c,d);
