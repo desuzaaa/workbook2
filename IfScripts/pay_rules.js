20a68ce8a82723f559d223d681c46e1fd2e5005f
Kameron de Suza <z6nekam@gmail.com>
2024-10-10 09:26:50 -0400

    pay rules again

--- IfScripts/pay_rules.js
+++ IfScripts/pay_rules.js
@@ -1,8 +1,12 @@
 var payRate = 25.0;
 
-var hoursWorked = 30;
+var hoursWorked = 45;
 
-if (hoursWorked > 40) {
+if (hoursWorked > 40)  {
+  var OTpay =
+  1.5 * payRate
+hoursWorked - 40
+
   console.log(hoursWorked);
 } else if (hoursWorked == 40) {
 } else {
