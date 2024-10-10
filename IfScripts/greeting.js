1ed6478f5074314cbfa0ba1b5ef107c7bd83ff1e
Kameron de Suza <z6nekam@gmail.com>
2024-10-10 13:35:39 -0400

    did greeting.js

---
+++ IfScripts/greeting.js
@@ -0,0 +1,14 @@
+var morning = "Good morning!"
+var afternoon = "Good afternoon!"
+var evening = "Good evening!"
+var timeOfDay = 23
+
+
+if (timeOfDay === 3){
+console.log("Good Morning!");
+} else if (timeOfDay === 15){
+console.log("Good Afternoon!");
+} else (timeOfDay === 20);{
+console.log("Good Afternoon!");
+}
+
