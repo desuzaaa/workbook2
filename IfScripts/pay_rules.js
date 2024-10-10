a2e6afd6aa3df392769e60f8dbb271dfa71931bd
Kameron de Suza <z6nekam@gmail.com>
2024-10-10 11:24:20 -0400

    did if else and pay rules

--- IfScripts/pay_rules.js
+++ IfScripts/pay_rules.js
@@ -1,14 +1,31 @@
-var payRate = 25.0;
-
+"use strict";
+var payRate = 17.30;
 var hoursWorked = 45;
+var regularHours = 40;
+var overtimePayrate = payRate * 1.5;
+var regularPay = 0;
+var overtimePay = 0;
+var overtimeHours = 0;
+var grossPay = 0;
 
-if (hoursWorked > 40)  {
-  var OTpay =
-  1.5 * payRate
-hoursWorked - 40
 
-  console.log(hoursWorked);
-} else if (hoursWorked == 40) {
+console.log(grossPay);
+
+overtimeHours = hoursWorked - regularHours
+
+console.log(overtimeHours);
+if (hoursWorked <= 40) {
+grossPay = payRate * hoursWorked
 } else {
-  console.log(payRate);
-}
+regularPay = regularHours * payRate
+overtimePay = overtimeHours * overtimePayrate
+grossPay =  regularPay + overtimePay
+ }
+
+
+
+
+
+
+
+console.log(grossPay);
---
+++ demos/if_else.js
@@ -0,0 +1,15 @@
+// Here's a simple exercise for you to practice using an if-else statement:
+
+// Exercise: Write a program that checks if a number is even or odd. If the number is even, print "The number is even." If the number is odd, print "The number is odd."
+
+// Hint: You can use the modulo operator (%) to check if a number is divisible by 2.
+
+var number = 9;
+
+var condition = (number % 2) == 0;
+
+if (condition) {
+console.log("The number is even");
+} else {
+console.log("The number is odd");  
+}
