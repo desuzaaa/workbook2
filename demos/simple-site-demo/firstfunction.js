4af4cc10fdcf28c838fd514e7639c7437a9d0437
Kameron de Suza <z6nekam@gmail.com>
2024-10-21 09:15:25 -0400

    bunch of check ins

---
+++ demos/simple-site-demo/firstfunction.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function test() {
+  console.log("test");
+  console.log("hi");
+  console.log("hi again");
+}
+test ()
---
+++ demos/simple-site-demo/secondfunction.js
@@ -0,0 +1,13 @@
+let messageDiv; 
+
+function indextest () {
+console.log("hello,");
+console.log("meu nome e kam.");
+console.log("como vai?");
+}
+indextest()
+
+function indextesttwo () {
+    console.log("I see you");
+}
+  indextesttwo();
